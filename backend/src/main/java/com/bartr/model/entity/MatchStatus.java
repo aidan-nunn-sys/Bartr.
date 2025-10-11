0eec91084d40d36dc71e6128055a679c677a3201
Aidan Nunn <awnunn@ncsu.edu>
2025-10-11 13:49:49 -0400

    Deleted backend

--- backend/src/main/java/com/bartr/model/entity/MatchStatus.java
+++
@@ -1,8 +0,0 @@
-package com.bartr.model.entity;
-
-public enum MatchStatus {
-    PENDING,     // Match found but not yet accepted by both parties
-    ACCEPTED,    // Both parties have accepted the match
-    REJECTED,    // One or both parties rejected the match
-    COMPLETED    // Trade has been completed
-}
